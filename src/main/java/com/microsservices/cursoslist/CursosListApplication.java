33b36671d95922cf0281b09fb696cc4f4e319f92
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-04-23 20:22:07 -0300

    Initial commit

---
+++ src/main/java/com/microsservices/cursoslist/CursosListApplication.java
@@ -0,0 +1,13 @@
+package com.microsservices.cursoslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microsservices/cursoslist/CursosListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservices.cursoslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
